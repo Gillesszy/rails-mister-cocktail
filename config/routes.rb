eae4a1a90ab3e005cbf94051e3078ce819a2e80a
gilles szymusiak <wippikas@hotmail.fr>
2017-05-18 15:16:12 +0200

    cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'ingredients/new'
+
+  get 'ingredients/create'
+
+  get 'ingredients/delete'
+
+  get 'doses/index'
+
+  get 'doses/show'
+
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/delete'
+
+  get 'cocktail/index'
+
+  get 'cocktail/show'
+
+  get 'cocktail/new'
+
+  get 'cocktail/create'
+
+  get 'cocktail/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
